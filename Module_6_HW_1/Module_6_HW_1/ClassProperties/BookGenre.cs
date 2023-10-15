02b3bd76f3a0d7f8dfc3efc7298a8e786d24b86f
Ilfoi22 <andrii.tsunivskii@gmail.com>
2023-10-15 16:55:27 +0300

    created-own-get-method

---
+++ Module_6_HW_1/Module_6_HW_1/ClassProperties/BookGenre.cs
@@ -0,0 +1,11 @@
+﻿namespace Module_6_HW_1.ClassProperties
+{
+    public class BookGenre
+    {
+        public int Id { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? BookGenreName { get; set; }
+    }
+}
